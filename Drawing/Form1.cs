7aa7eb5d41eaf6123b3ac43dd39f502327b2f167
João Victor Pereira da Silva <vics@ynko.onmicrosoft.com>
2021-08-05 00:54:31 -0300

    Rainbows

--- Drawing/Form1.cs
+++ Drawing/Form1.cs
@@ -13,6 +13,7 @@ namespace MathDraw
 {
     public partial class MainForm : Form
     {
+        Point[] pontos = new Point[1200];
         Random rng = new Random();
         public MainForm()
         {
@@ -21,11 +22,16 @@ namespace MathDraw
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            drawtest();
+            //    Drawtest();
+            for (int i = 0; i <= 100; i++)
+            {
+                DrawtestColor();
+               
+            }
             
         }
 
-        public void drawtest()
+        public void Drawtest()
         {
             new Thread(() =>
             {
@@ -33,7 +39,7 @@ namespace MathDraw
                 Bitmap bitmap1 = new Bitmap(1000, 800, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
                 Graphics graphics1 = Graphics.FromImage(bitmap1);
                 Pen pen1 = new Pen(Color.Black, 1);
-                Point[] pontos = new Point[1200];
+               
                 int limit = 900;
                 for (int i = 0; i < pontos.Length - limit; i++)
                 {
@@ -55,7 +61,44 @@ namespace MathDraw
             }).Start();
             
         }
+        public void DrawtestColor()
+        {
+            new Thread(() =>
+            {
+                Color rainbow = Color.FromArgb(255, rng.Next(0, 255), rng.Next(0, 255), rng.Next(0, 255));
+                Thread.CurrentThread.IsBackground = true;
+                Bitmap bitmap1 = new Bitmap(1000, 800, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+                Graphics graphics1 = Graphics.FromImage(bitmap1);
+                Pen pen1 = new Pen(rainbow, 1);
+                pen1.Color = rainbow;
 
+
+                int limit = 10;
+                for (int i = 0; i < pontos.Length - limit; i++)
+                {
+                    pontos[i].X = rng.Next(0, pictureBox1.Width);
+                    pontos[i].Y = rng.Next(0, pictureBox1.Height);
+                    if (i % 100 == 0)
+                    {
+                        rainbow = Color.FromArgb(255, rng.Next(0, 255), rng.Next(0, 255), rng.Next(0, 255));
+                        pen1.Color = rainbow;
+                    }
+                }
+                
+                graphics1.DrawPolygon(pen1, pontos);
+
+                _ = pictureBox1.Invoke(new MethodInvoker(
+              delegate ()
+              {
+                  pictureBox1.Image = bitmap1;
+                  pictureBox1.Update();
+                 bitmap1.Dispose();
+                 graphics1.Dispose();
+                  Thread.Sleep(100);
+              }));
+            }).Start();
+
+        }
         private void pictureBox1_Click(object sender, EventArgs e)
         {
             keepdrawing();
@@ -65,9 +108,9 @@ namespace MathDraw
           
             for (int i = 0; i < 100; i++)
             {
-
-                drawtest();
-
+                
+                DrawtestColor();
+             
             }
          
 
